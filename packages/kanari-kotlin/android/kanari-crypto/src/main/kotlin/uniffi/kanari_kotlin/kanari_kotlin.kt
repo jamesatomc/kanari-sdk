2d661391eaff64829b0a9c42f09f80b0c177e6ac
jamesatomc <jiraphonwansai2002@gmail.com>
2026-09-09 23:26:34 +0700

    chore: Update dependencies to latest versions in Cargo.toml and Cargo.lock

--- packages/kanari-kotlin/android/kanari-crypto/src/main/kotlin/uniffi/kanari_kotlin/kanari_kotlin.kt
+++ packages/kanari-kotlin/android/kanari-crypto/src/main/kotlin/uniffi/kanari_kotlin/kanari_kotlin.kt
@@ -844,34 +844,34 @@ private fun uniffiCheckContractApiVersion(lib: IntegrityCheckingUniffiLib) {
 }
 @Suppress("UNUSED_PARAMETER")
 private fun uniffiCheckApiChecksums(lib: IntegrityCheckingUniffiLib) {
-    if (lib.uniffi_kanari_kotlin_checksum_func_blake3_hash_api() != 40879) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_blake3_hash_api() and 0xFFFF) != 40879) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_derive_keypair_from_mnemonic() != 64620) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_derive_keypair_from_mnemonic() and 0xFFFF) != 64620) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_derive_keypair_from_path_api() != 58579) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_derive_keypair_from_path_api() and 0xFFFF) != 58579) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_derive_multiple_addresses_api() != 5280) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_derive_multiple_addresses_api() and 0xFFFF) != 5280) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_generate_keypair_api() != 14350) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_generate_keypair_api() and 0xFFFF) != 14350) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_generate_mnemonic_api() != 55169) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_generate_mnemonic_api() and 0xFFFF) != 55169) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_import_keypair_from_private_key() != 40818) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_import_keypair_from_private_key() and 0xFFFF) != 40818) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_list_supported_curves() != 8770) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_list_supported_curves() and 0xFFFF) != 8770) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_sign_message_api() != 38481) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_sign_message_api() and 0xFFFF) != 38481) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
-    if (lib.uniffi_kanari_kotlin_checksum_func_verify_signature_api() != 18576) {
+    if ((lib.uniffi_kanari_kotlin_checksum_func_verify_signature_api() and 0xFFFF) != 18576) {
         throw RuntimeException("UniFFI API checksum mismatch: try cleaning and rebuilding your project")
     }
 }
